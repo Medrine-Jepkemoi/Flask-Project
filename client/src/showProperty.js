f6500d00ab1569093863822c27e076b72b28323d
Lewis Ndegwa <lewiskanyi77@gmail.com>
2023-07-06 15:23:08 +0300

    show property

--- client/src/showProperty.js
+++ client/src/showProperty.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import './HomeCard.css'; 
 
 const HomeCard = ({ home }) => {
   const [showReviewForm, setShowReviewForm] = useState(false);
