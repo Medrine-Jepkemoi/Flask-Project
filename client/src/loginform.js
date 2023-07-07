a5bc25e052845b22dac65f6f00022ba4ee4f1334
lew-monk <lewmonk@gmail.com>
2023-07-07 11:01:39 +0300

    Fixed Compat Issues

--- client/src/loginform.js
+++ client/src/loginform.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { loginUser } from './api';
+import React, { useState } from "react";
+import { loginUser } from "./api";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -16,14 +16,16 @@ const LoginForm = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
+    let user = new FormData();
+    user.append("email", email);
+    user.append("password", password);
     try {
-      const data = await loginUser(email, password);
+      const data = await loginUser(user);
       console.log(data); // Handle successful login
       // Reset the form
-      setEmail('');
-      setPassword('');
-      setError('');
+      setEmail("");
+      setPassword("");
+      setError("");
     } catch (error) {
       setError(error.error); // Display the login error
     }
@@ -31,16 +33,25 @@ const LoginForm = () => {
 
   return (
     <div>
-      
       {error && <p>{error}</p>}
       <form onSubmit={handleLogin}>
         <div>
           <label>Email:</label>
-          <input type="email" value={email} onChange={handleEmailChange} required />
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
         </div>
         <div>
           <label>Password:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} required />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
         </div>
         <button type="submit">Login</button>
       </form>
